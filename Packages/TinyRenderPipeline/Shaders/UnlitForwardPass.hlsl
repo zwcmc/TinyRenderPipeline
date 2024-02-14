70a0dc11b026ab263e5857adaaa08dd25ba02a32
chenzwsss <746869297@qq.com>
2024-02-14 16:31:42 +0800

    Remove GPU Instancing support in unlit shader

--- Packages/TinyRenderPipeline/Shaders/UnlitForwardPass.hlsl
+++ Packages/TinyRenderPipeline/Shaders/UnlitForwardPass.hlsl
@@ -5,22 +5,17 @@ struct Attributes
 {
     float3 positionOS : POSITION;
     float2 uv         : TEXCOORD0;
-    UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
 struct Varyings
 {
     float2 uv         : TEXCOORD0;
     float4 positionCS : SV_POSITION;
-    UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
 Varyings UnlitVertex(Attributes input)
 {
     Varyings output = (Varyings)0;
-
-    UNITY_SETUP_INSTANCE_ID(input);
-    UNITY_TRANSFER_INSTANCE_ID(input, output);
 
     output.uv = TRANSFORM_TEX(input.uv, _BaseMap);
 
@@ -30,8 +25,6 @@ Varyings UnlitVertex(Attributes input)
 
 half4 UnlitFragment(Varyings input) : SV_TARGET
 {
-    UNITY_SETUP_INSTANCE_ID(input);
-
     half4 texColor = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, input.uv);
     half3 color = texColor.rgb * _BaseColor.rgb;
     half alpha = texColor.a * _BaseColor.a;
