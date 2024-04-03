258d1f25331401fb993511fe290de851f07e5c26
zw16c <746869297@qq.com>
2024-04-03 15:25:51 +0800

    Code optimizations

--- Packages/TinyRenderPipeline/Runtime/Data/TinyRenderPipelineAsset.cs
+++ Packages/TinyRenderPipeline/Runtime/Data/TinyRenderPipelineAsset.cs
@@ -5,36 +5,43 @@ using UnityEngine.Rendering;
 [CreateAssetMenu(menuName = "Rendering/Tiny Render Pipeline Asset")]
 public class TinyRenderPipelineAsset : RenderPipelineAsset
 {
-    [Serializable]
-    private struct MainLightShadow
+    public enum ShadowResolution
     {
-        public ShadowResolution shadowResolution;
-
-        [Range(1, 4)]
-        public int cascadeCount;
-
-        [Range(0.0f, 1.0f)]
-        public float cascadeRatio1, cascadeRatio2, cascadeRatio3;
+        _1024 = 1024,
+        _2048 = 2048,
+        _4096 = 4096
     }
 
     [Serializable]
-    private struct AdditionalLightsShadow
+    private class MainLightShadow
     {
-        public ShadowResolution shadowResolution;
+        public ShadowResolution shadowResolution = ShadowResolution._4096;
+
+        [Range(1, 4)]
+        public int cascadeCount = 4;
+
+        [Range(0.0f, 1.0f)]
+        public float cascadeRatio1 = 0.067f, cascadeRatio2 = 0.2f, cascadeRatio3 = 0.467f;
+    }
+
+    [Serializable]
+    private class AdditionalLightsShadow
+    {
+        public ShadowResolution shadowResolution = ShadowResolution._2048;
     }
 
     // Shadows
     [Serializable]
-    private struct Shadows
+    private class Shadows
     {
-        public float shadowDistance;
+        public float shadowDistance = 50.0f;
 
         [Range(0.0f, 1.0f)]
-        public float cascadeBorder;
+        public float cascadeBorder = 0.2f;
 
-        public MainLightShadow mainLightShadow;
+        public MainLightShadow mainLightShadow = default;
 
-        public AdditionalLightsShadow additionalLightsShadow;
+        public AdditionalLightsShadow additionalLightsShadow = default;
     }
 
     [SerializeField]
@@ -44,37 +51,10 @@ public class TinyRenderPipelineAsset : RenderPipelineAsset
     private bool m_SupportsHDR = true;
 
     [SerializeField]
-    private Shadows m_Shadows = new Shadows
-    {
-        shadowDistance = 150.0f,
-        cascadeBorder = 0.2f,
-        mainLightShadow = new MainLightShadow
-        {
-            shadowResolution = ShadowResolution._2048,
-            cascadeCount = 4,
-            cascadeRatio1 = 0.067f,
-            cascadeRatio2 = 0.2f,
-            cascadeRatio3 = 0.467f
-        },
-        additionalLightsShadow = new AdditionalLightsShadow
-        {
-            shadowResolution = ShadowResolution._2048
-        }
-    };
+    private Shadows m_Shadows = default;
 
     [SerializeField]
     private PostProcessingData m_PostProcessingData = default;
-
-    public enum ShadowResolution
-    {
-        _256 = 256,
-        _512 = 512,
-        _1024 = 1024,
-        _2048 = 2048,
-        _4096 = 4096
-    }
-
-    public TinyRenderPipeline renderPipeline;
 
     public bool useSRPBatcher
     {
@@ -113,6 +93,8 @@ public class TinyRenderPipelineAsset : RenderPipelineAsset
     }
 
     public PostProcessingData postProcessingData => m_PostProcessingData;
+
+    public TinyRenderPipeline renderPipeline;
 
     public override Type pipelineType => renderPipeline.GetType();
 
