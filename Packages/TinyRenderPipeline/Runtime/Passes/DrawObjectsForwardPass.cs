2498adf6fb3e7c1c8df4406f1cde70a22061baae
zw16c <746869297@qq.com>
2024-07-18 15:52:29 +0800

    Update DrawObjectsForwardPass
    1. Add DrawRenderGraphObjects method to render opaque and transparent objects in Render Graph way;
    2. Replace obsolete ScriptableRenderContext.DrawRenderers with CommandBuffer.DrawRendererList;

--- Packages/TinyRenderPipeline/Runtime/Passes/DrawObjectsForwardPass.cs
+++ Packages/TinyRenderPipeline/Runtime/Passes/DrawObjectsForwardPass.cs
@@ -1,5 +1,7 @@
+using UnityEngine.Experimental.Rendering;
 using UnityEngine.Experimental.Rendering.RenderGraphModule;
 using UnityEngine.Rendering;
+using UnityEngine.Rendering.RendererUtils;
 
 public class DrawObjectsForwardPass
 {
@@ -8,9 +10,23 @@ public class DrawObjectsForwardPass
 
     private bool m_IsOpaque;
 
+    private class PassData
+    {
+        public RendererList rendererList;
+        public RendererListHandle rendererListHandle;
+    }
+
+    private PassData m_PassData;
+
+    private static void ExecutePass(RasterCommandBuffer cmd, RendererList rendererList)
+    {
+        cmd.DrawRendererList(rendererList);
+    }
+
     public DrawObjectsForwardPass(bool isOpaque = false)
     {
         m_IsOpaque = isOpaque;
+        m_PassData = new PassData();
     }
 
     public void Render(ScriptableRenderContext context, ref RenderingData renderingData)
@@ -25,13 +41,10 @@ public class DrawObjectsForwardPass
             var sortFlags = m_IsOpaque ? SortingCriteria.CommonOpaque : SortingCriteria.CommonTransparent;
             var drawingSettings = RenderingUtils.CreateDrawingSettings(ref renderingData, sortFlags);
             var filteringSettings = m_IsOpaque ? new FilteringSettings(RenderQueueRange.opaque) : new FilteringSettings(RenderQueueRange.transparent);
-            context.DrawRenderers(renderingData.cullResults, ref drawingSettings, ref filteringSettings);
+
+            RenderingUtils.CreateRendererList(context, ref renderingData, drawingSettings, filteringSettings, ref m_PassData.rendererList);
+            ExecutePass(CommandBufferHelpers.GetRasterCommandBuffer(cmd), m_PassData.rendererList);
         }
-    }
-
-    private class PassData
-    {
-
     }
 
     public void DrawRenderGraphObjects(RenderGraph renderGraph, TextureHandle colorTarget, TextureHandle depthTarget, TextureHandle mainLightShadowmap, TextureHandle additionalLightsShadowmap, ref RenderingData renderingData)
@@ -39,7 +52,25 @@ public class DrawObjectsForwardPass
         var sampler = m_IsOpaque ? m_DrawOpaqueObjectsSampler : m_DrawTransparentObjectsSampler;
         using (var builder = renderGraph.AddRasterRenderPass<PassData>(sampler.name, out var passData, sampler))
         {
-            
+            if (colorTarget.IsValid())
+                builder.UseTextureFragment(colorTarget, 0, IBaseRenderGraphBuilder.AccessFlags.Write);
+            if (depthTarget.IsValid())
+                builder.UseTextureFragmentDepth(depthTarget, IBaseRenderGraphBuilder.AccessFlags.Write);
+
+            var sortFlags = m_IsOpaque ? SortingCriteria.CommonOpaque : SortingCriteria.CommonTransparent;
+            var filteringSettings = m_IsOpaque ? new FilteringSettings(RenderQueueRange.opaque) : new FilteringSettings(RenderQueueRange.transparent);
+            DrawingSettings drawingSettings = RenderingUtils.CreateDrawingSettings(ref renderingData, sortFlags);
+
+            RenderingUtils.CreateRendererListWithRenderGraph(renderGraph, ref renderingData, drawingSettings, filteringSettings, ref passData.rendererListHandle);
+
+            builder.UseRendererList(passData.rendererListHandle);
+
+            builder.AllowPassCulling(false);
+
+            builder.SetRenderFunc((PassData data, RasterGraphContext rasterGraphContext) =>
+            {
+                ExecutePass(rasterGraphContext.cmd, data.rendererListHandle);
+            });
         }
     }
 }
